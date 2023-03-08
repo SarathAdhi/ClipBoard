a4500a35b2ad78ab70d779daaf4fb0ae6af65df7
SarathAdhi <s.sarathadhithya@gmail.com>
2023-03-08 20:49:41 +0530

    navlink for redirecting home page

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -2,7 +2,14 @@ const Navbar = () => {
   return (
     <header className="w-full flex flex-col items-center bg-gray-300">
       <div className="w-full max-w-[1280px] p-2 sm:p-4">
-        <h1 className="underline">ClipBoard</h1>
+        <h1
+          className="underline cursor-pointer"
+          onClick={() => {
+            location.replace("/");
+          }}
+        >
+          ClipBoard
+        </h1>
       </div>
     </header>
   );
